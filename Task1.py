0937d9c1d8a56a600ff7f507b283ccfd145731b8
Vitalii <vitalii.fedkiv.25@kse.org.ua>
2026-04-03 14:28:55 +0300

    Оптимізовано формулу обрахунку результату

--- Task1.py
+++ Task1.py
@@ -2,7 +2,7 @@ my_poly = {0: -10, 2: 3, 4: 1}
 def evaluate_polynomial(poly_dict, x):
     result = 0
     for p, k in poly_dict.items():
-        result = result + k*(x**p)
+        result += k * (x ** p)
     return result
 result1 = evaluate_polynomial(my_poly,2)
 print(result1)
