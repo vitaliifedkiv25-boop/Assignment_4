94e44c0743b22dc029ebaac899d968650046cb51
Vitalii <vitalii.fedkiv.25@kse.org.ua>
2026-04-03 15:09:46 +0300

    Написана основна частина задачі

---
+++ Task2.py
@@ -0,0 +1,16 @@
+students_math_results = [
+{"name": "Олександр", "scores": {"Calculus": 85, "Algebra": 90, "Discrete Math": 78}},
+{"name": "Марія", "scores": {"Calculus": 65, "Algebra": 55, "Discrete Math": 70}},
+{"name": "Іван", "scores": {"Calculus": 92, "Algebra": 88, "Discrete Math": 95}},
+{"name": "Анна", "scores": {"Calculus": 45, "Algebra": 60, "Discrete Math": 50}}
+]
+successful_student = {}
+def get_successful_students(students_list, passing_score=60):
+    for student in students_list:
+        name = student["name"]
+        scores = student["scores"]
+        if student["scores"] >= passing_score:
+            average = sum(scores) / len(scores)
+            successful_student[student["name"]] = average
+    return successful_student
+print(get_successful_students(students_math_results))
